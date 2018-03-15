2565cfa5a86000b127e810260e38bd7d4d8182cc
norowaretta <36477899+norowaretta@users.noreply.github.com>
2018-03-15 10:48:07 +0300

    Add files via upload

--- modulshop/sell.spec.js
+++ modulshop/sell.spec.js
@@ -1,5 +1,67 @@
-var generator = new RandGenerator();
-
+var sell = new Modulshop();
+sell.model=
+{
+	"name":"Магазин_хозтоваров",
+	"top_arrow":[
+		{
+			"name":"Прищепка"	
+		}
+	],
+	"bottom_arrow":[
+	
+	    {
+	         "name":"Склад",
+	         "properties":[
+	           {
+                 	"count":20
+     	       }
+		    ]
+		},
+		{
+		    "name":"Сканеры",
+			"properties":[
+			   {
+                 	"count":20
+     	       }
+		    ]
+		  },
+		  	{
+		    "name":"Сотрудников",
+			"properties":[
+			   {
+                 	"count":20
+     	       }
+		    ]
+		  },
+		  
+		 {
+		    "name":"Товары",
+			"properties":[
+			   {
+                 	"count":20
+     	       }
+		    ]
+		  } 		  		  	 	  	  
+		],
+		"left_arrow": [
+		       {
+			   "name":"Заявоки_на_исполнение"
+			   }
+			
+		],
+		"right_arrow":[
+		      {
+			   "name":"Исполненные_заявки"
+			   
+		      }
+	    ],
+		"left_arrow": [
+		       {
+			   "name":"Регламент_условия"
+			   }
+	]	
+}
+console.log(sell.model)
 //проверяем правильно ли работает генератор чисел
 describe("Генератор случайных чисел", function() {
 
@@ -7,14 +69,14 @@ describe("Генератор случайных чисел", function() {
   //  assert.equal((rand_value >=20 && rand_value <= 30), true);
 
 
-  it("Подсчитывает число выполненных продаж за ? минут каждого сотрудника", function() {
+  it("Подсчитывает число выполненных продаж время работы каждого сотрудника для вычисления эффективнсти работы сотрудника и работы в дальнейшем", function() {
 	  
-	  var prodanotovar, timestart, timeend, timezatrata, prodavec_id, 
+	  var prodazha, prodanotovar, timestart, timeend, timezatrata, prodavec_id, effectivnost, plannaprodazhy                                                    
 	  
 	  
 	  
 	
 	  
-	  )
+  })
 
 });
