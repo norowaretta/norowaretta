a701effc8c1f1656375d68488d201cfa4381576e
norowaretta <36477899+norowaretta@users.noreply.github.com>
2018-03-15 10:29:35 +0300

    Add files via upload

---
+++ modulshop/sell.spec.js
@@ -0,0 +1,20 @@
+var generator = new RandGenerator();
+
+//проверяем правильно ли работает генератор чисел
+describe("Генератор случайных чисел", function() {
+
+
+  //  assert.equal((rand_value >=20 && rand_value <= 30), true);
+
+
+  it("Подсчитывает число выполненных продаж за ? минут каждого сотрудника", function() {
+	  
+	  var prodanotovar, timestart, timeend, timezatrata, prodavec_id, 
+	  
+	  
+	  
+	
+	  
+	  )
+
+});
