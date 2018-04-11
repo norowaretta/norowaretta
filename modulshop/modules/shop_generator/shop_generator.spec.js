5f9a58183f9814843782e28913e65af300aa23ac
norowaretta <36477899+norowaretta@users.noreply.github.com>
2018-04-11 13:41:45 +0300

    Add files via upload

---
+++ modulshop/modules/shop_generator/shop_generator.spec.js
@@ -0,0 +1,82 @@
+var sell = new Modulshop();
+sell.model=
+{
+	"name":"Магазин_хозтоваров",
+	"top_arrow":[
+		{
+			"name":"Прищепка"	
+		}
+	],
+	"bottom_arrow":[
+	
+	    {
+	         "name":"Склад",
+	         "properties":[
+	           {
+                 	"count":20
+     	       }
+		    ]
+		},
+		{
+		    "name":"Сканеры",
+			"properties":[
+			   {
+                 	"count":20
+     	       }
+		    ]
+		  },
+		  	{
+		    "name":"Сотрудников",
+			"properties":[
+			   {
+                 	"count":20
+     	       }
+		    ]
+		  },
+		  
+		 {
+		    "name":"Товары",
+			"properties":[
+			   {
+                 	"count":20
+     	       }
+		    ]
+		  } 		  		  	 	  	  
+		],
+		"left_arrow": [
+		       {
+			   "name":"Заявоки_на_исполнение"
+			   }
+			
+		],
+		"right_arrow":[
+		      {
+			   "name":"Исполненные_заявки"
+			   
+		      }
+	    ],
+		"left_arrow": [
+		       {
+			   "name":"Регламент_условия"
+			   }
+	]	
+}
+console.log(sell.model)
+//проверяем правильно ли работает генератор чисел
+describe("Генератор случайных чисел", function() {
+
+
+  //  assert.equal((rand_value >=20 && rand_value <= 30), true);
+
+
+  it("Подсчитывает число выполненных продаж время работы каждого сотрудника для вычисления эффективнсти работы сотрудника и работы в дальнейшем", function() {
+	  
+	  var prodazha, prodanotovar, timestart, timeend, timezatrata, prodavec_id, effectivnost, plannaprodazhy                                                    
+	  
+	  
+	  
+	
+	  
+  })
+
+});
