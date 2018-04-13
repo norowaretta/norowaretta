cd143dcb553df377eb48a5fc44b13d2e68657295
norowaretta <36477899+norowaretta@users.noreply.github.com>
2018-04-13 17:48:37 +0300

    Add files via upload

---
+++ modulshop/modules/shop/shoptest.spec.js
@@ -0,0 +1,61 @@
+var data =
+{
+	"name":"shoptest",
+	"top_arrow":[
+		{
+			"name":"rules"
+		}
+	],
+	"bottom_arrow":[
+		{
+			"name":"magazin",
+		},
+		{
+			"name":"potok",
+			
+		},
+
+	],
+	"left_arrow":[
+		{
+			"name":"application",
+			"properties":[
+				{
+					"kolvo":80
+				},
+				{
+					"time":11
+				}
+			]
+		}
+	],
+	"right_arrow":[
+		{
+			"name":"rezultat"
+		}
+	]
+}
+
+var sizepotok = new CalcPotok();
+sizepotok.model = data;
+console.log(sizepotok.model)
+
+describe("Проходимость магазина", function() {
+
+  it("Если у нас есть ТЦ и на долю отдельного магазина приходится поток 80 людей в час, то поток всего за 11 часов работы составит 880 людей", function() {
+	
+	/* console.log("1"); */
+	
+  	var potokchas = sizepotok.model.left_arrow[0].properties[0].kolvo;
+	var timework = sizepotok.model.left_arrow[0].properties[1].time;
+	
+	console.log(potokchas, timework);
+	
+	var totalpotok = sizepotok.Calc(potokchas,timework)
+	
+	
+	console.log(totalpotok);
+		
+    assert.equal((totalpotok == 880), true);
+  });
+});
